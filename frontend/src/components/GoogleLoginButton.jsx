4c311f407f6fc38385f4fedc7cae0eb9895ab793
igordoomofficial <igordoomofficial@gmail.com>
2026-01-05 11:09:19 -0300

    modifying googleLogin button urls

--- frontend/src/components/GoogleLoginButton.jsx
+++ frontend/src/components/GoogleLoginButton.jsx
@@ -6,7 +6,7 @@ export function GoogleLoginButton() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
-                    redirectTo: `${window.location.origin}/dashboard`
+                    redirectTo: `${window.location.origin}`
                 }
             })
             if (error) throw error
